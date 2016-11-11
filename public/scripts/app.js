f3fe4ddf300b3e93e0bf7765108d1a382b718cd8
virginiachae <chae.virginia@gmail.com>
2016-11-10 23:07:20 -0800

    got cakes to show up on front end with handlebars and to display new cake in front end when created

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,23 @@
 console.log("Sanity Check: JS is working!");
-
+var template;
+var allCakes = [];
 $(document).ready(function(){
+
+	var source = $('#cake-li-template').html();
+			template = Handlebars.compile(source);
+
+
+	$.ajax({
+        method: 'GET',
+        url: '/api/cakes',
+				data: $(this).serializeArray(),
+        success: handleSuccess,
+        // error: handleError
+    });
 
 	$('#newCakeForm').on('submit', function(e) {
 	    e.preventDefault();
-	    console.log('new book serialized', $(this).serializeArray());
+	    console.log('new cake serialized', $(this).serializeArray());
 	    $.ajax({
 	      method: 'POST',
 	      url: '/api/cakes',
@@ -13,9 +26,30 @@ $(document).ready(function(){
 	      // error: newCakeError
 	    });
 	  });
+		function render() {
+			$('#cake-list').empty();
+
+			for (var i = 0; i < allCakes.length; i++) {
+				var cakesHtml = template({
+					name : allCakes[i].name,
+					cake : allCakes[i].cake,
+					frosting : allCakes[i].frosting,
+					description : allCakes[i].description
+				})
+
+				$('#cake-list').append(cakesHtml)
+		}
+	}
+
+	function handleSuccess(json) {
+			allCakes = json;
+			render();
+			}
+
 
 	function newCakeSuccess(cake) {
-		console.log(cake);
+		allCakes.push(cake);
+		render();
 		$('#newCakeForm')[0].reset()
 	}
 
--- server.js
+++ server.js
@@ -109,6 +109,6 @@ app.post('/api/cakes', function(req, res) {
  **********/
 
 // listen on port 3000
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Express server is up and running on http://localhost:3000/');
+app.listen(process.env.PORT || 3030, function () {
+  console.log('Express server is up and running on http://localhost:3030/');
 });
